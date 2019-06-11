d3fffe0bf117585c641e6c56336442ff7d873be7
Nabeel <tmnabeel30@gmail.com>
2019-06-11 11:50:50 +0530

    Resolved Multiple Select problem

---
+++ Todoey/ItemModel.swift
@@ -0,0 +1,17 @@
+//
+//  ItemModel.swift
+//  Todoey
+//
+//  Created by Nabeel on 6/11/19.
+//  Copyright © 2019 Nabeel. All rights reserved.
+//
+
+import Foundation
+
+class ItemModel{
+    
+    var title: String = ""
+    var Done: Bool = false
+    
+    
+}
--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,36 +10,67 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     // Variable are declared here
-    var itemARRAY = [String?]()
-    var lastSelectedRow: NSIndexPath? = nil
+    var itemARRAY = [ItemModel]()
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        tableView.dataSource = self
-        tableView.delegate = self
-        if let item = defaults.array(forKey: "ToDoList") as? [String]{
-            itemARRAY = item
-            tableView.reloadData()
-        }
-        print(itemARRAY)
+       
+        let newItem = ItemModel()
+        newItem.title = "mike"
+        itemARRAY.append(newItem)
+        
+        let newItem2 = ItemModel()
+        newItem2.title = "angela"
+        itemARRAY.append(newItem2)
+
+        
+        let newItem3 = ItemModel()
+        newItem3.title = "nabeel"
+        itemARRAY.append(newItem3)
+        
+        
+//        if let item = defaults.dictionaryWithValues(forKey: "ToDoList") as? [String]{
+//            itemARRAY = item
+//            tableView.reloadData()
+//        }
+//        print(itemARRAY)
+//    }
+////
     }
-    
     //MARK - TableviewDataSource Method
+    
+    //This is function is first called
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
-        cell.textLabel?.text = itemARRAY[indexPath.row]
+        let trueOrfalse = itemARRAY[indexPath.row].Done
+
+        cell.textLabel?.text = itemARRAY[indexPath.row].title
+        if trueOrfalse == true{
+           cell.accessoryType = .checkmark
+            
+            print("clicked")
+        }else{
+     cell.accessoryType = .none
+           
+            print("clicked")
+        }
+       
         
         return cell
         
         
     }
+    
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return itemARRAY.count
     }
+    
+
     
     //Mark - TableView Delegate Method
     
@@ -47,36 +78,62 @@ class ToDoListViewController: UITableViewController {
         
         
         //print item in index row
-        print(itemARRAY[indexPath.row])
+        print(itemARRAY[indexPath.row].title)
+        
+    
+        //remove grey colour when selected or Deselects things
+         tableView.deselectRow(at: indexPath, animated: true)
         
         //Check and uncheck
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        tableView.deselectRow(at: indexPath, animated: true)
+         itemARRAY[indexPath.row].Done = !(itemARRAY[indexPath.row].Done)
+        print(itemARRAY[indexPath.row].Done,itemARRAY[indexPath.row].title)
+   
+        tableView.reloadData()
+        
+
+       
+        
+       
     }
+    
+    
+    
+    
+    
+    
     @IBAction func Add(_ sender: UIBarButtonItem) {
         var textField = UITextField()
-                let alert = UIAlertController(title: "Add Todoey", message: "", preferredStyle: .alert)
+        
+        let alert = UIAlertController(title: "Add Todoey", message: "", preferredStyle: .alert)
+        
         let alertAction = UIAlertAction(title: "Add New Todoey", style: .default) { (alert) in
+        
             print("Sucesssul")
+           
             
-            if textField.text != nil{
-                self.itemARRAY.append(textField.text!)
-                self.tableView.reloadData()
-                  self.defaults.set(self.itemARRAY, forKey: "ToDoList")
+            
+            let nItem = ItemModel()
+            
+            nItem.title = textField.text!
          
-            }
+            
+            
+            self.itemARRAY.append(nItem)
+//            self.defaults.set(self.itemARRAY, forKey: "ToDoList")
+            self.tableView.reloadData()
+            
+//            if textField.text != nil{
+//                self.itemARRAY.append(newItem.title)
+//                self.tableView.reloadData()
+//                  self.defaults.set(self.itemARRAY, forKey: "ToDoList")
+//
+//            }
           
         }
     
         
         
-        
+  //Mark - UIAlert
         //telling alert to show action
         alert.addAction(alertAction)
         
@@ -85,7 +142,6 @@ class ToDoListViewController: UITableViewController {
         alert.addTextField { (alertTextfield) in
             alertTextfield.placeholder = "Create new action"
             textField = alertTextfield
-        
             
             
         }
@@ -100,5 +156,6 @@ class ToDoListViewController: UITableViewController {
     
 
 }
+
 
 
